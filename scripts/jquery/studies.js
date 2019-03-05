0f1ab1d4b0fb10d381f48e4a1ce688fc56fe0de4
ThanhQuang <tranthanhquang47@gmail.com>
2019-03-05 11:27:25 +0700

    5/3

--- scripts/jquery/studies.js
+++ scripts/jquery/studies.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
   var myTd = $('#my-id');
+  var trs = $('tr');
+  var hightlighteds = $('.highlighted');
+  var tds = $('td');
 
-  $('td').on('click',function(event){
+  $('tr').on('mouseover',function(event){
     event.preventDefault();
     $('.highlighted').removeClass('highlighted');
     $(this).addClass('highlighted');
-  })
+  });
+
+  $('tr').on('mouseout',function(event){
+    event.preventDefault();
+    $('.highlighted').removeClass('highlighted');
+    $(this).addClass('highlighted');
+  });
+
+  $('td').on('mouseover',function(event){
+    event.preventDefault();
+    $('#my-id').html('<span>' + $(this).text() + '</span>');
+  });
+  
+  $('td').on('mouseout',function(event){
+    event.preventDefault();
+    $('#my-id').html(null);
+  });
 });
+
 /* $(function(){
   alert("Quang.");
 })*/
--- scripts/studies.js
+++ scripts/studies.js
@@ -17,3 +17,23 @@ window.onload= function(){
     });
   }
 }
+
+window.onload= function(){
+  //alert("Hello!");
+  var trs = document.getElementsByTagName('tr');
+  var hightlighteds = document.getElementsByClassName('hightlighted');
+  var myTd = document.getElementById('my-id');
+
+  for(var i = 0; i < trs.length; i++){
+    trs[i].addEventListener('mouseover', function(event){
+      event.preventDefault();
+
+      var highlighteds = document.getElementsByClassName('highlighted');
+
+      for(var i = highlighteds.length - 1;i >= 0; i--){
+        highlighteds[i].classList.remove('highlighted');
+      }
+      this.classList.add('highlighted');
+    });
+  }
+}
