4e230854ffcda06eb4dd241d1827de131dcf7e52
ThanhQuang <tranthanhquang47@gmail.com>
2019-03-08 10:46:21 +0700

    8/3/2019

--- scripts/jquery/index.js
+++ scripts/jquery/index.js
@@ -2,5 +2,18 @@
 
 $(function(){
   // logScreenSize();
-  resizeElements(['.my-button-people-name', '.my-button-jump-to-people'])
+  resizeElements(['.my-button-people-name', '.my-button-jump-to-people']);
+  $('.my-button-people-name').on('click', function(){
+    $(this).toggleClass('btn-secondary btn-danger');
+    var $button = $(this).closest('div').find('.my-button-jump-to-people');
+    $button.toggleClass('d-none');
+
+    resizeElements(['.my-button-people-name', '.my-button-jump-to-people']);
+
+    var id = $button.find('a').attr('href');
+    $(id).toggleClass('d-none');
+  });
+  if(!modernizr.mq('(min-width: 576px)')){
+    $('.my-button-people-name').css('width','180px');
+  }
 });
